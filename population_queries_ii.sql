dec56f80eb306914cf126eafbee6d9a579e12d4c
Mahin Sugrim <mahinsugrim@gmail.com>
2020-03-04 00:26:30 -0400

    Answer for Q5

--- population_queries_ii.sql
+++ population_queries_ii.sql
@@ -10,3 +10,11 @@ LEFT JOIN population_years
   ON countries.id =
   population_years.country_id
 WHERE continent = 'Oceania' AND year = 2005;
+
+-- Q5 - What is the average population of countries in South America in 2003?
+SELECT AVG(population) AS 'Average South American population in 2003'
+FROM countries
+LEFT JOIN population_years
+  ON countries.id =
+  population_years.country_id
+WHERE continent = 'South America' AND year = 2003;
