bec9eea650b2f6721a45aaeb0c521d46b68c299b
Mahin Sugrim <mahinsugrim@gmail.com>
2020-03-04 00:25:53 -0400

    Answer for Q4

--- population_queries_ii.sql
+++ population_queries_ii.sql
@@ -3,3 +3,10 @@ SELECT COUNT(*) AS 'number of African countries'
 FROM countries
 WHERE continent = 'Africa';
 
+-- Q4 - What was the total population of Oceania in 2005?
+SELECT SUM(population) AS 'Total Oceania population in 2005'
+FROM countries
+LEFT JOIN population_years
+  ON countries.id =
+  population_years.country_id
+WHERE continent = 'Oceania' AND year = 2005;
